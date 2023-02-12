9f30f00db45e8585830c763bac1fd1d2d580cd5e
rohaansayed <119560435+rohaansayed@users.noreply.github.com>
2023-02-12 14:09:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ const Fruits = () => {
   return (
     <div id="fruits">
       { /* Change code below this line */ }
-      <Typesoffruit />
+      <Typesoffruits />
 
       { /* Change code above this line */ }
     </div>
